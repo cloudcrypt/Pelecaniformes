1da8cf35a31511f0d9e5f3844b64b9426a58ef2d
Carlin <CarlinLiu@users.noreply.github.com>
2017-04-11 15:44:18 -0600

    Added hasParent2

--- pelecaniformes.pl
+++ pelecaniformes.pl
@@ -74,7 +74,26 @@ hasParent(falcinellus,plegadis).
 hasParent(chihi,plegadis).
 hasParent(ajaja,platalea). 
 
-%hasParent2(A,B) :-
+hasParentC(pelecanus,pelecanus_erythrorhynchos).
+hasParentC(pelecanus,pelecanus_occidentalis).
+hasParentC(botaurus,botaurus_lentiginosus).
+hasParentC(ixobrychus,ixobrychus_exilis).
+hasParentC(ardea,ardea_herodias).
+hasParentC(ardea,ardea_alba).
+hasParentC(egretta,egretta_thula).
+hasParentC(egretta,egretta_caerulea).
+hasParentC(egretta,egretta_tricolor).
+hasParentC(egretta,egretta_rufescens).
+hasParentC(bubulcus,bubulcus_ibis).
+hasParentC(butorides,butorides_virescens).
+hasParentC(nycticorax,nycticorax_nycticorax).
+hasParentC(nyctanassa,nyctanassa_violacea).
+hasParentC(eudocimus,eudocimus_albus).
+hasParentC(plegadis,plegadis_falcinellus).
+hasParentC(plegadis,plegadis_chihi).
+hasParentC(platalea,platalea_ajaja).
+
+hasParent2(A,B) :- (hasCompoundName(G,S,A) -> hasParentC(B,A)) ; hasParent(A,B).
 
 commonName(pelecanus,pelican).
 commonName(botaurus,bittern).
@@ -112,7 +131,7 @@ commonName(ajaja,roseateSpoonbill).
 %hasCommonName(N,C) :- (order(N);family(N);genus(N)), commonName(N,C).
 %hasCommonName(N,C) :- commonName(X,C), hasParent(Y,X), hasCompoundName(Y,X,N).
 
-%hasSciName(C,N) :-
+hasSciName(C,N) :- (hasCompoundName(X, Y, N) -> commonName(Y,C) ); commonName(N,C).
 
 hasCompoundName(G,S,N) :- genus(G), species(S), hasParent(S,G), atom_concat('_',S,Z), atom_concat(G,Z,N).
 
@@ -122,8 +141,12 @@ hasCompoundName(G,S,N) :- genus(G), species(S), hasParent(S,G), atom_concat('_',
 
 %synonym(A, B) :-
 
-countSpecies(A, 1) :- hasCompundName(G,S,A).
-countSpecies(A,N) :- genus(A) -> hasParent(X, A), species(X), Z is N - 1, countSpecies(A,Z).
+
+countSpecies(A, 0) :-
+
+%countSpecies(A, 1) :- hasCompundName(G,S,A).
+%countSpecies(A, N) :- genus(A) -> 
+%countSpecies(A, N) :- aggregate_all(N, species(A),Count).
 
 %rangesTo
 rangesTo(pelecanus_erythrorhynchos,alberta).
